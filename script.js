d349a0fc5821256d2536a7ecbad457e2b51e8c59
OndrejKabrt <126494487+OndrejKabrt@users.noreply.github.com>
2024-11-24 20:37:40 +0100

    All fixed

--- script.js
+++ script.js
@@ -1,91 +1,96 @@
 const socket = io();
 const editorContainer = document.getElementById('editorContainer');
 const connectedUsers = document.getElementById('connectedUsers');
-
 let cursor;
 let username;
 let userid;
+let curtext;
 
-
+//odchozí
+// přidá k do listu users user se zadaným jmenem
 document.getElementById('join').onclick = function() {
-username = document.getElementById('username').value;
+    username = document.getElementById('username').value;
     if (username) {
         socket.emit('new user', username);
         const inputfield = document.getElementById("username");
         inputfield.remove();
         const button = document.getElementById("join");
         button.remove();
-        socket.emit('connect');
-
-
+        newcursor();
+        cursor = document.getElementById(userid);
+        socket.emit('create cursor',{cursor:userid})
+        const status = document.getElementById("status");
+        status.innerText = "Připojeno";
+        editorContainer.setAttribute('contenteditable', 'true');
     }
 };
 
-
-editorContainer.addEventListener('input', () => {
+//odchozí
+//pošle editovaný text 
+editorContainer.addEventListener('input', () => { 
     socket.emit('text change', { text: editorContainer.innerText });
 });
 
-
+//příchozí
+//update textu podle příchozího
 socket.on('document update', (text) => {
     editorContainer.innerText = text;
 });
 
-
+//příchozí
+//upraví list useru
 socket.on('user list', (users) => {
     connectedUsers.innerHTML = 'Připojení uživatelé: ' + users.map(user => user.name).join(', ');
 });
 
-
-socket.on('connect', () => {
-    status.innerText = 'Připojeno ke serveru';
-});
-
-
-socket.on('disconnect', () => {
-    status.innerText = 'Odpojeno od serveru';
-});
-
-
 socket.on('create cursor', (data) => {
-    createcursor(data.userid)
-})
+    newcursor(data.userid);
+});
 
 
+//odchozí
+//vezme pozici myši a pošle ji
 editorContainer.addEventListener('mousemove', (event) => {
-    cursor.style.left = event.clientX + 'px';
-    cursor.style.top = event.clientY + 'px';
-    cursor.style.display = 'block';
-    socket.emit('cursor update', { position: { x: event.clientX, y: event.clientY } });
+    if(userid){
+        cursor.style.left = event.clientX + 'px';
+        cursor.style.top = event.clientY + 'px';
+        cursor.style.display = 'block';
+        socket.emit('cursor update', { position: { x: event.clientX, y: event.clientY },cursor: userid });
+    }
 });
 
 
+//příchozí
+//updatuje pozicic miši
 socket.on('cursor update', (data) => {
-    cursor = document.getElementById(data.userid);
-    cursor.style.left = data.position.x + 'px';
-    cursor.style.top = data.position.y + 'px';
-    cursor.style.display = 'block';
+    console.log(data.userid)
+    othercursor  = document.getElementById(data.id);
+    othercursor.style.left = data.position.x + 'px';
+    othercursor.style.top = data.position.y + 'px';
+    othercursor.style.display = 'block';
 });
 
-function createcursor(id){
+//vytvoří nový cursor
+function newcursor(color){
     const newDiv = document.createElement("div");
     document.body.appendChild(newDiv);
-    if (!id){
-        colour = generate_colour();
+    if(!color){
+        color = generate_color();
+        console.log("color created")
         newDiv.className = "cursor";
-        newDiv.style["background-colour"]= colour;
-        newDiv.setAttribute('id',colour);
-        userid = colour;
+        newDiv.style["background-color"]= color;
+        newDiv.setAttribute('id',color)
+        userid = color;
     }else{
+        console.log("color created")
         newDiv.className = "cursor";
-        newDiv.style["background-colour"]= colour;
-        newDiv.setAttribute('id',colour);
+        newDiv.style["background-color"]= color;
+        newDiv.setAttribute('id',color)
     }
 }
 
-
-//Z kodu Adama Hlavačika
-function generate_colour(){
+//generuje barvu
+function generate_color(){
     var letters = "0123456789ABCDEF"; 
     var color = '#';
     for(let i = 0; i<6;i++){
--- server.js
+++ server.js
@@ -7,19 +7,25 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 let users = [];
-//let documentText = "";
+let documentText = "";
 
 app.use(express.static(__dirname));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Serve index.html
+});
 
+
+//při připojení
 io.on('connection', (socket) => {
     console.log('A user connected');
-    
-    // Přidání nového uživatele
-    socket.on('new user', (username) => {
+
+       // Přidání nového uživatele
+       socket.on('new user', (username) => {
         users.push({ id: socket.id, name: username });
         io.emit('user list', users);
-        io.emit('document update', documentText); // Pošleme dokument novému uživateli
+        io.emit('document update', documentText);
+       // io.emit('cursor update')
     });
 
     // Příjem úpravy textu
@@ -28,23 +34,28 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('document update', documentText);
     });
 
+
     // Zpracování odpojení uživatele
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        console.log('A user disconnected'/* + users.id*/);
         users = users.filter(user => user.id !== socket.id);
         io.emit('user list', users);
     });
 
+
     // Zpracování kurzoru
     socket.on('cursor update', (data) => {
-        socket.broadcast.emit('cursor update', { id: socket.id, position: data.position });
+        socket.broadcast.emit('cursor update', { id: data.cursor, position: data.position });
+    });
+
+    socket.on('create cursor', (data) => {
+        socket.broadcast.emit('create cursor', { userid: data.cursor });
     });
 });
 
-const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(8080, () => {
+    console.log(`Server is running on port 8080`);
 });
 
 
